8cc069944935abaca9d32a1e5f2c69a2c24588d8
Roozbeh Shafiee <roozbeh.shafiee@gmail.com>
2019-03-10 00:47:01 +0100

    fix start.sh issue and run docker compose as container

--- start.sh
+++ start.sh
@@ -5,13 +5,14 @@ read DOMAIN
 echo "Enter the email address you want to Let's Encrypt ssl certificates issue for:"
 read EMAIL
 
-sed -i 's/example.tld/$DOMAIN/g' ./conf/auth_server/auth.yml
-sed -i 's/example.tld/$DOMAIN/g' ./docker-compose.yml
-sed -i 's/example.tld/$DOMAIN/g' ./conf/traefik/traefik.toml
-sed -i 's/example.tld/$DOMAIN/g' ./conf/registry/config.yml
-sed -i 's/email@example.tld/$EMAIL/g' ./conf/traefik/traefik.toml
+sed -i "s|example.tld|${DOMAIN}|g" ./conf/auth_server/auth.yml
+sed -i "s|example.tld|${DOMAIN}|g" ./docker-compose.yml
+sed -i "s|example.tld|${DOMAIN}|g" ./conf/traefik/traefik.toml
+sed -i "s|example.tld|${DOMAIN}|g" ./conf/registry/config.yml
+sed -i "s|email@example.tld|${EMAIL}|g" ./conf/traefik/traefik.toml
 
-docker-compose up -d
+
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/rootfs/$PWD" -w="/rootfs/$PWD" docker/compose:1.23.2 up -d
 echo ""
 echo ""
 echo "SERVICE DEPLOYED SUCCESSFULLY!"
