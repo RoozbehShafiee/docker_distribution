7fa1a6dabb1aef47614478e6a6e9a887d1bbb1e0
Roozbeh Shafiee <roozbeh.shafiee@gmail.com>
2019-03-10 20:46:52 +0100

    take services non tls behind lb

--- start.sh
+++ start.sh
@@ -4,14 +4,9 @@ echo "Enter the domain address you want to deploy service on (e.g. infunity.clou
 read DOMAIN
 echo "Enter the email address you want to Let's Encrypt ssl certificates issue for:"
 read EMAIL
-
-sed -i "s|example.tld|${DOMAIN}|g" ./conf/auth_server/auth.yml
+sed -i "s|email@example.tld|${EMAIL}|g" ./docker-compose.yml
 sed -i "s|example.tld|${DOMAIN}|g" ./docker-compose.yml
-sed -i "s|example.tld|${DOMAIN}|g" ./conf/traefik/traefik.toml
-sed -i "s|example.tld|${DOMAIN}|g" ./conf/registry/config.yml
-sed -i "s|email@example.tld|${EMAIL}|g" ./conf/traefik/traefik.toml
-
-
+sed -i "s|example.tld|${DOMAIN}|g" ./registry/config.yml
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/rootfs/$PWD" -w="/rootfs/$PWD" docker/compose:1.23.2 up -d
 echo ""
 echo ""
