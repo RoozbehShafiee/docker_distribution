65bb1ebbf6d9f8cbf58f982d6f28d9710f777f5a
Roozbeh Shafiee <roozbeh.shafiee@gmail.com>
2019-03-10 21:21:48 +0100

    moving ssl cert generating out of container

--- auth_server/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-/bin/ash
-
-if [ ! -f /etc/auth_server/ssl/certificate.crt ]
-then
-  openssl genrsa -out /etc/auth_server/ssl/private.key 4096 -nodes
-  openssl req -new -x509 -key /etc/auth_server/ssl/private.key -sha256 -config /etc/ssl/ssl_gen.cnf -out /etc/auth_server/ssl/certificate.crt -days 36500
-fi
-
-/usr/local/bin/auth_server /etc/auth_server/auth.yml
--- start.sh
+++ start.sh
@@ -4,9 +4,17 @@ echo "Enter the domain address you want to deploy service on (e.g. infunity.clou
 read DOMAIN
 echo "Enter the email address you want to Let's Encrypt ssl certificates issue for:"
 read EMAIL
+
 sed -i "s|email@example.tld|${EMAIL}|g" ./docker-compose.yml
 sed -i "s|example.tld|${DOMAIN}|g" ./docker-compose.yml
 sed -i "s|example.tld|${DOMAIN}|g" ./registry/config.yml
+
+if [ ! -f volumes/ssl/certificate.crt ]
+then
+  openssl genrsa -out volumes/ssl/private.key 4096 -nodes
+  openssl req -new -x509 -key volumes/ssl/private.key -sha256 -config ssl_gen.cnf -out volumes/ssl/certificate.crt -days 36500
+fi
+
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/rootfs/$PWD" -w="/rootfs/$PWD" docker/compose:1.23.2 up -d
 echo ""
 echo ""
